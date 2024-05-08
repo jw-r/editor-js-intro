e0e15c2d8aa1e7f6ce44509593f2e0cf0bef2214
류정우 <evencoding@gmail.com>
2024-05-09 07:26:10 +0900

    chore: procode 제거

--- src/components/editor.tsx
+++ src/components/editor.tsx
@@ -1,24 +1,26 @@
 "use client";
 
 import "@blocknote/core/fonts/inter.css";
-import {
-  BlockNoteSchema,
-  defaultBlockSpecs,
-  filterSuggestionItems,
-} from "@blocknote/core";
-import {
-  SuggestionMenuController,
-  getDefaultReactSlashMenuItems,
-  useCreateBlockNote,
-} from "@blocknote/react";
+import { BlockNoteSchema, defaultBlockSpecs } from "@blocknote/core";
+import { useCreateBlockNote } from "@blocknote/react";
 import { BlockNoteView } from "@blocknote/shadcn";
 import "@blocknote/shadcn/style.css";
-import { CodeBlock, insertCode } from "@defensestation/blocknote-code";
 
 const initialContent = [
   {
     type: "heading",
     content: "문서 제목",
+  },
+  {
+    type: "paragraph",
+  },
+  {
+    type: "paragraph",
+    content: "/ 입력하면 다양한 것 테스트할 수 있음",
+  },
+  {
+    type: "paragraph",
+    content: "입력한 글자를 드레그해도 tool 확인 가능",
   },
   {
     type: "paragraph",
@@ -87,17 +89,12 @@ const initialContent = [
       },
     ],
   },
-  {
-    type: "procode",
-    content: "대충 코드 블록",
-    text: "대충 코드 블록",
-  },
 ] as any;
 
 const schema = BlockNoteSchema.create({
   blockSpecs: {
     ...defaultBlockSpecs,
-    procode: CodeBlock as any,
+    // procode: CodeBlock as any,
   },
 });
 
@@ -107,17 +104,5 @@ export default function Editor() {
     initialContent,
   });
 
-  return (
-    <BlockNoteView editor={editor} theme="light">
-      <SuggestionMenuController
-        triggerCharacter="/"
-        getItems={async (query) =>
-          filterSuggestionItems(
-            [...getDefaultReactSlashMenuItems(editor), insertCode()],
-            query
-          )
-        }
-      />
-    </BlockNoteView>
-  );
+  return <BlockNoteView editor={editor} theme="light" />;
 }
