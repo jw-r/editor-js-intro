71c00ccaaf3e7dcc185dc217837fdd7bd2c7f8f9
류정우 <evencoding@gmail.com>
2024-05-10 07:48:46 +0900

    feat: create manager

--- src/app/(routes)/remirror/page.tsx
+++ src/app/(routes)/remirror/page.tsx
@@ -2,6 +2,9 @@
 
 import dynamic from 'next/dynamic'
 
+// const TutorialEditor = dynamic(() => import('@/components/remirror-editor/tutorial'), {
+//   ssr: false,
+// })
 const RemirrorEditor = dynamic(() => import('@/components/remirror-editor'), {
   ssr: false,
 })
@@ -10,6 +13,8 @@ export default function Remirror() {
   return (
     <div>
       <RemirrorEditor />
+      <div style={{ width: 'full', height: '10px', backgroundColor: 'blue', padding: '10px' }} />
+      {/* <TutorialEditor /> */}
     </div>
   )
 }
--- src/components/remirror-editor/index.tsx
+++ src/components/remirror-editor/index.tsx
@@ -1,28 +1,34 @@
-import { OnChangeJSON } from '@remirror/react'
-import { WysiwygEditor } from '@remirror/react-editors/wysiwyg'
-import { useCallback, useState } from 'react'
-import { RemirrorJSON } from 'remirror'
+import { useRemirror } from '@remirror/react'
+import { BoldExtension, CalloutExtension, ItalicExtension } from 'remirror/extensions'
 
-const STORAGE_KEY = 'remirror-editor-content'
+// const remirrorJsonFormStorage = {
+//   type: 'doc',
+//   content: [
+//     { type: 'heading', attrs: { level: 1 }, content: [{ type: 'text', text: 'Hello world' }] },
+//     {
+//       type: 'paragraph',
+//       content: [
+//         { type: 'text', text: 'Hello' },
+//         { type: 'text', marks: [{ type: 'italic' }], text: 'word' },
+//       ],
+//     },
+//   ],
+// }
 
-export default function RemirrorEditor() {
-  const [initialContent] = useState<RemirrorJSON | undefined>(() => {
-    const content = localStorage.getItem(STORAGE_KEY)
-    return content ? JSON.parse(content) : undefined
+export default function MyEditor() {
+  const { manager } = useRemirror({
+    extensions: () => [
+      new BoldExtension(),
+      new ItalicExtension(),
+      new CalloutExtension({ defaultType: 'warn' }),
+    ],
+
+    content: '<p>I love <b>Remirror</b></p>',
+
+    selection: 'start',
+
+    stringHandler: 'html',
   })
 
-  const handleEditorChange = useCallback((json: RemirrorJSON) => {
-    localStorage.setItem(STORAGE_KEY, JSON.stringify(json))
-  }, [])
-
-  return (
-    <div style={{ padding: '16px' }}>
-      <WysiwygEditor
-        placeholder="다양한 마크다운 문법을 사용해서 테스트해보세요!"
-        initialContent={initialContent}
-      >
-        <OnChangeJSON onChange={handleEditorChange} />
-      </WysiwygEditor>
-    </div>
-  )
+  return <div>myeditor</div>
 }
